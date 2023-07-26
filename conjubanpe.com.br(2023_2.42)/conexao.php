fcab715e4d3da8f6e3d913e546b0e7fe3b1b34af
Gscaio <57861909+Gscaio@users.noreply.github.com>
2023-07-26 13:00:18 -0300

    Add files via upload

---
+++ conjubanpe.com.br(2023_2.42)/conexao.php
@@ -0,0 +1,9 @@
+<?php
+	$servidor = "localhost";
+	$usuario = "u727873447_conjubanbd";
+	$senha = "Aa99241386";
+	$dbname = "u727873447_conjubanbd";
+	
+	//Criar a conexao
+	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
+?>
